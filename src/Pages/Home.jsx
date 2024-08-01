61530973cbe6ed1500482ebcda6b25dcdb779404
Harshitha Shetty <97578545+Harshitha-S-Shetty@users.noreply.github.com>
2024-08-01 21:23:13 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
   return (
-    <div style={{backgroundImage:"url('img.png')", backgroundSize: 'cover', width: '100%', height:'500px'}}>
-
+    <div 
+      style={{
+        backgroundImage: "url('https://blogimages.softwaresuggest.com/blog/wp-content/uploads/2023/04/14124110/Top-10-Job-Portals-in-India-That-Makes-Them-Good-min.jpg')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        width: '100%',
+        height: '590px',
+        '@media (max-width: 768px)': {
+          height: '400px',
+        }
+      }}
+    >
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
