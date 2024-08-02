9842bdaab2cfd5ac6e8d9d46510cec772bda6d32
Harshitha Shetty <97578545+Harshitha-S-Shetty@users.noreply.github.com>
2024-08-02 11:33:07 +0530

    Update PostJob.jsx

--- src/Pages/PostJob.jsx
+++ src/Pages/PostJob.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { useTheme } from '@mui/material/styles';
@@ -9,12 +8,12 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import Button from '@mui/material/Button';
-import { useNavigate } from 'react-router-dom';   //8.navigation insted of link tag
+import { useNavigate } from 'react-router-dom'; //8.navigation instead of link tag
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
 const MenuProps = {
-  PaperProps: {  
+  PaperProps: {
     style: {
       maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
       width: 250,
@@ -43,7 +42,7 @@ function PostJob() {
   const [experienceLevel, setExperienceLevel] = useState("");
   const [jobDescription, setJobDescription] = useState("");
 
-  const nav=useNavigate()  //8.
+  const nav = useNavigate(); //8.
 
   const theme = useTheme();
 
@@ -59,67 +58,83 @@ function PostJob() {
     setExperienceLevel(event.target.value);
   };
 
-
-  // 2.getting initial value
+  // Getting initial value
   let initialvalue;
-  if(localStorage.getItem("JobList")==null){
-    initialvalue=[]
-  }else{
-    initialvalue=JSON.parse(localStorage.getItem("JobList"))
+  if (localStorage.getItem("JobList") === null) {
+    initialvalue = [];
+  } else {
+    initialvalue = JSON.parse(localStorage.getItem("JobList"));
   }
- // JSON.parse is used to remove the string value that is stored in localstroge of getitem
 
+  const [JobList, setJobList] = useState(initialvalue);
+  console.log(JobList);
 
-  const [JobList,setJobList]=useState(initialvalue)
-  console.log(JobList)
-
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    var JobList_id=101         // if we add recipe_id 
-    JobList_id=JobList.length > 0 ? JobList[JobList.length - 1]["JobList_id"] + 1 : JobList_id;
-    console.log(JobList_id)
-    const new_data={        //4.merge all statevariable inside the new_data
-      JobList_id:JobList_id,
-      jobtitle:jobtitle,
-      company:company,
-      workplaceType:workplaceType,
-      location:location,   
-      experienceLevel:experienceLevel,
-      jobDescription:jobDescription
-    }   
-    console.log(new_data) 
+    var JobList_id = 101; // If we add recipe_id
+    JobList_id = JobList.length > 0 ? JobList[JobList.length - 1]["JobList_id"] + 1 : JobList_id;
+    console.log(JobList_id);
+    const new_data = {
+      JobList_id: JobList_id,
+      jobtitle: jobtitle,
+      company: company,
+      workplaceType: workplaceType,
+      location: location,
+      experienceLevel: experienceLevel,
+      jobDescription: jobDescription,
+    };
+    console.log(new_data);
 
-    setJobList([...JobList,new_data]) //5.
+    setJobList([...JobList, new_data]); //5.
 
-    await localStorage.setItem("JobList", JSON.stringify(JobList)) //6.
+    await localStorage.setItem("JobList", JSON.stringify(JobList)); //6.
 
-    alert("Job List Added Successfully") //8.
+    alert("Job List Added Successfully"); //8.
 
-    nav("/viewjob") //8.
+    nav("/viewjob"); //8.
   };
 
-  useEffect(()=>{
-    localStorage.setItem("JobList", JSON.stringify(JobList)) //7.
-  },[JobList])
+  useEffect(() => {
+    localStorage.setItem("JobList", JSON.stringify(JobList)); //7.
+  }, [JobList]);
 
   return (
-    <div style={{ backgroundImage: "url('https://media.istockphoto.com/id/1331579485/vector/background-curved-light-blue.jpg?s=612x612&w=0&k=20&c=Wslr-PIxcQDoxXmzC7_w8rbFM9_s_5Jz99tE3ftNM1A=')", backgroundSize: 'cover', backgroundPosition: 'center', minHeight: '100vh' }}>
-      <h4 style={{ textAlign: "center", fontSize: "25px", marginTop: "10px", marginBottom: "10px", color:"grey", fontWeight:"lighter" }}>Post a Job</h4>
+    <div
+      style={{
+        backgroundImage: "url('https://media.istockphoto.com/id/1331579485/vector/background-curved-light-blue.jpg?s=612x612&w=0&k=20&c=Wslr-PIxcQDoxXmzC7_w8rbFM9_s_5Jz99tE3ftNM1A=')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: '100vh',
+      }}
+    >
+      <h4
+        style={{
+          textAlign: "center",
+          fontSize: "25px",
+          marginTop: "10px",
+          marginBottom: "10px",
+          color: "grey",
+          fontWeight: "lighter",
+        }}
+      >
+        Post a Job
+      </h4>
       <form onSubmit={handleSubmit}>
         <Box
           sx={{
-            width: 400,
+            width: { xs: 300, sm: 400 },
             maxWidth: '100%',
             margin: "auto",
             display: "flex",
             flexDirection: "column",
             gap: "20px",
-           
+            padding: { xs: "10px", sm: "20px" }, // Add padding for smaller screens
+            backgroundColor: "rgba(255, 255, 255, 0.8)", // Semi-transparent background
           }}
         >
           <TextField fullWidth label="Job Title" onChange={(e) => setJobTitle(e.target.value)} value={jobtitle} />
           <TextField fullWidth label="Company" onChange={(e) => setCompany(e.target.value)} value={company} />
-          <FormControl sx={{ m: 0, width: 400 }}>
+          <FormControl sx={{ m: 0, width: '100%' }}>
             <InputLabel id="workplace-type-label required">Workplace type</InputLabel>
             <Select
               labelId="workplace-type-label"
@@ -130,17 +145,13 @@ function PostJob() {
               MenuProps={MenuProps}
             >
               {names.map((name) => (
-                <MenuItem
-                  key={name}
-                  value={name}
-                  style={getStyles(name, workplaceType, theme)}
-                >
+                <MenuItem key={name} value={name} style={getStyles(name, workplaceType, theme)}>
                   {name}
                 </MenuItem>
               ))}
             </Select>
           </FormControl>
-          <FormControl sx={{ m: 0, width: 400 }}>
+          <FormControl sx={{ m: 0, width: '100%' }}>
             <InputLabel id="location-label">Location</InputLabel>
             <Select
               labelId="location-label"
@@ -151,17 +162,13 @@ function PostJob() {
               MenuProps={MenuProps}
             >
               {namess.map((name) => (
-                <MenuItem
-                  key={name}
-                  value={name}
-                  style={getStyles(name, location, theme)}
-                >
+                <MenuItem key={name} value={name} style={getStyles(name, location, theme)}>
                   {name}
                 </MenuItem>
               ))}
             </Select>
           </FormControl>
-          <FormControl sx={{ m: 0, width: 400 }}>
+          <FormControl sx={{ m: 0, width: '100%' }}>
             <InputLabel id="experience-level-label">Experience Level</InputLabel>
             <Select
               labelId="experience-level-label"
@@ -172,11 +179,7 @@ function PostJob() {
               MenuProps={MenuProps}
             >
               {namesss.map((name) => (
-                <MenuItem
-                  key={name}
-                  value={name}
-                  style={getStyles(name, experienceLevel, theme)}
-                >
+                <MenuItem key={name} value={name} style={getStyles(name, experienceLevel, theme)}>
                   {name}
                 </MenuItem>
               ))}
